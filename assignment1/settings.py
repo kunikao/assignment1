fc285b40681f3dc3338a6eed6ba20d184fe9d35f
20232426 <kaku009@ess.ais.ac.nz>
2024-07-14 08:51:43 +1200

    settings updated

--- assignment1/settings.py
+++ assignment1/settings.py
@@ -136,3 +136,9 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
