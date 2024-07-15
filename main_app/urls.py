8e5b8de14442b624765edcaa3983e4ff09898f7a
20232426 <kaku009@ess.ais.ac.nz>
2024-07-16 10:54:35 +1200

    a

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from .views import RegisterAPIView, LoginAPIView, MainFormAPIView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
-    path('login/', LoginAPIView.as_view(), name='login'),
+    path('auth/', LoginAPIView.as_view(), name='login'),
     path('mainform/', MainFormAPIView.as_view(), name='mainform')
 ]
