c87086f38d2d3e0ec053abee2768496e0e915b1e
20232426 <kaku009@ess.ais.ac.nz>
2024-07-16 08:50:45 +1200

    a

--- assignment1/urls.py
+++ assignment1/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('main_app.urls')),
-    #path('auth/', obtain_auth_token)
+    path('auth/', obtain_auth_token)
 ]
