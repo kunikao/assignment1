061225c1f9cb5d8e13635429c0d0ec32173f61fe
20232426 <kaku009@ess.ais.ac.nz>
2024-07-15 14:30:01 +1200

    changes

--- assignment1/settings.py
+++ assignment1/settings.py
@@ -89,13 +89,23 @@ WSGI_APPLICATION = 'assignment1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'm8nSEJg6VXIP',
+        'HOST': 'ep-curly-sound-a76lu128.ap-southeast-2.aws.neon.tech',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
